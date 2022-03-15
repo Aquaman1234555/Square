4aa26685167a97483d3bee2f0ed27f0a39103b0a
Aquaman1234555 <101315879+Aquaman1234555@users.noreply.github.com>
2022-03-15 15:18:14 +0530

    Add files via upload

---
+++ scratch_canvas (3).py
@@ -0,0 +1,16 @@
+import turtle
+turtle.pensize(10)
+for i in range(2):
+    (turtle.shape(''))
+    #Write the shape as square
+    turtle.pencolor('blue')
+    #Write the pen color as red
+    turtle.forward(200)
+    turtle.left(90)
+for i in range(2):
+    (turtle.shape(''))
+    #Write the shape as circle
+    turtle.pencolor('red')
+    #Write the pen color as green
+    turtle.forward(200)
+    turtle.left(90)
